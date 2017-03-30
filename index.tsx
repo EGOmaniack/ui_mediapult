0df1b650e36cf699cc49df097c9e1aec056bb01c
EGOmaniack <EGOmaniack@mail.ru>
2017-03-30 21:38:30 +1000

    чет не взлетело

---
+++ index.tsx
@@ -0,0 +1,3 @@
+import * as checkbox from "checkbox";
+
+export checkbox;
---
+++ newcheckbox/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import * as DOM from 'react-dom';
+import styled from 'styled-components';
+
+const Outer = styled.div`
+   width: fit-content;
+
+   >label {
+       color: green;
+   }
+`
+
+const Check = styled.input`
+   color: green;
+   background: black;
+`
+
+export class Newcheckbox extends React.Component<any, any> {
+    constructor(props: any){
+        super(props);
+    }
+    render() {
+        return <Outer>
+                    <input id="check1" type="checkbox" className="checkbox"/>
+                    <label htmlFor="check1" >{this.props.name}</label>
+                </Outer>;
+    }
+}
